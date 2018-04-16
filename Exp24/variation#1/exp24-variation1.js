9b547c4e0f7329d3c2284ce013ac8a22b9289a52
Treisy Jiménez Vega <treisyjimenez@gmail.com>
2018-04-16 13:44:53 -0600

    Create variation#1

--- Exp24/variation#1/exp24-variation1.js
+++ Exp24/variation#1/exp24-variation1.js
@@ -1,2 +1,44 @@
 let showSticky = 3000;
 let hideSticky = 13200;
+let currentScroll;
+
+const $stickyShareTools = document.querySelector('.sticky-share-tools');
+const $newIconsSticky = document.querySelector('.sticky-share-wrapper .sticky-share ul.new-icons');
+const $iconsSticky = document.querySelector('.sticky-share-wrapper .sticky-share ul');
+
+const $stickyHeaderTemplate =  ``;
+
+const $stickyBannerTemplate = `<div class="sticky-banner-container">hola sticky</div>`;
+
+function changeText() {
+    let title = doccument.querySelector('header h1.headline').innerHTML;
+
+    document.querySelector('.sticky-banner-title').innerHTML = title;
+}
+
+function addStickyBanner() {
+    let container = document.querySelector('main');
+
+    let template = document.createElement($stickyBannerTemplate);
+
+    document.querySelector('.article-closer').insertBefore(template, container);
+    
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    changeText();
+    addStickyBanner();
+    
+    window.addEventListener('scroll', function() {
+        currentScroll = window.scrollY || document.documentElement.scrollTop;
+
+        if ( $stickyShareTools.classList.contains('_show') ) {
+            console.log('tiene show');
+        }
+
+        if ( currentScroll >= showSticky ) {
+            
+        }
+        console.log(currentScroll);
+    })
+}, false);
--- dist/Exp24/variation#1/exp24-variation1.js
+++ dist/Exp24/variation#1/exp24-variation1.js
@@ -1,5 +1,44 @@
-"use strict";
+'use strict';
 
 var showSticky = 3000;
 var hideSticky = 13200;
+var currentScroll = void 0;
+
+var $stickyShareTools = document.querySelector('.sticky-share-tools');
+var $newIconsSticky = document.querySelector('.sticky-share-wrapper .sticky-share ul.new-icons');
+var $iconsSticky = document.querySelector('.sticky-share-wrapper .sticky-share ul');
+
+var $stickyHeaderTemplate = '';
+
+var $stickyBannerTemplate = '<div class="sticky-banner-container">hola sticky</div>';
+
+function changeText() {
+    var title = doccument.querySelector('header h1.headline').innerHTML;
+
+    document.querySelector('.sticky-banner-title').innerHTML = title;
+}
+
+function addStickyBanner() {
+    var container = document.querySelector('main');
+
+    var template = document.createElement($stickyBannerTemplate);
+
+    document.querySelector('.article-closer').insertBefore(template, container);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    changeText();
+    addStickyBanner();
+
+    window.addEventListener('scroll', function () {
+        currentScroll = window.scrollY || document.documentElement.scrollTop;
+
+        if ($stickyShareTools.classList.contains('_show')) {
+            console.log('tiene show');
+        }
+
+        if (currentScroll >= showSticky) {}
+        console.log(currentScroll);
+    });
+}, false);
 //# sourceMappingURL=exp24-variation1.js.map
